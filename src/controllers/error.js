5bfc2ea254586402aa07c229235d685f03b70e03
Ali <ali.dahdouh@gmail.com>
2020-02-18 14:59:50 +0200

    created error handles with css and html files for them relates #7

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,1 +1,12 @@
 // handle client and server errors
+const path = require('path');
+
+exports.client = (req, res) =>{
+    res.status(404)
+    res.sendFile(Path.join(__dirname, '..', '..','public','404.html'));
+}
+
+exports.server = (err, req, res, next) =>{
+    res.status(500)
+    res.sendFile(Path.join(__dirname, '..','..','public','500.html'))
+}
