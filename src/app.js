91f750b7489c62a910535d4b340c9d79f7c0aa81
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-18 14:57:20 +0200

    create server and listen to the port relates #2

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 // here the main page we will import express, compression, ...
 // here will set PORT, disable(powered by),compression, handle staic, import other handle
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+
+const app = express();
+
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 4000);
+app.use(compression());
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+module.exports = app;
