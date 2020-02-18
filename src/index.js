b5ab3c03ffdb378ca8aed8e36f888ffa1973238a
Ali <ali.dahdouh@gmail.com>
2020-02-18 16:11:13 +0200

    added node-fetch and dotenv to test data coming from api relates #10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // app entry point to run server
 const app = require('./app');
+const fetch = require('node-fetch')
+
+//// testing fetch from api (to remove)
+
+const dotenv = require('dotenv').config();
+const apiKey = dotenv.parsed.API_KEY;
+
+fetch(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=test&api_key=${apiKey}&format=json&limit=3`)
+.then(res => res.json())
+.then(res => console.log(res.results.albummatches.album[0].artist))
+
+//// testing fetch from api (to remove)
+
 
 app.listen(app.get('port'), () => {
  console.log(`http://localhost:${app.get('port')}`)
