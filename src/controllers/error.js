97edaed3398723f4c9f554039069e4ec171a4566
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-19 15:46:32 +0200

    handle 500 error server on clinet site relates #23

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,11 +1,12 @@
 const Path = require('path');
 
-exports.client = (req, res) =>{
-    res.status(404)
-    res.sendFile(Path.join(__dirname, '..', '..','public','404.html'));
-}
+exports.client = (req, res) => {
+  res.status(404);
+  res.sendFile(Path.join(__dirname, '..', '..', 'public', '404.html'));
+};
 
-exports.server = (err, req, res, next) =>{
-    res.status(500)
-    res.sendFile(Path.join(__dirname, '..','..','public','500.html'))
-}
+exports.server = (err, req, res) => {
+  console.log('error 500');
+  res.status(500);
+  res.sendFile(Path.join(__dirname, '..', '..', 'public', '500.html'));
+};
