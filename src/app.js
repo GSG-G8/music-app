a2617570396af7604d1a9e9d29341c88f587277e
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-19 10:47:02 +0200

    complete fetch function and fetch-node relates #15

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ const controllers = require('./controllers')
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.disable('x-powered-by');
 app.set('port', process.env.PORT || 4000);
 app.use(compression());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use(controllers)
--- src/controllers/search.js
+++ src/controllers/search.js
@@ -3,13 +3,17 @@ const dotenv = require('dotenv').config();
 
 
 const search = (req,res)=>{
-  console.log(req.body.name)
+  const query = req.body.input;
 
-  const url = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=test&api_key=${process.env.API_KEY}&format=json&limit=3`;
+  const url = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${query}&api_key=${process.env.API_KEY}&format=json&limit=10`;
   fetch(url)
-    .then(res => res.json())
-    // .then(res => console.log(res.results.albummatches.album[1].artist))
-    .catch(err => res.status(500));
+    .then(result => result.json())
+    .then(result => {
+      const ourresult = result.results.albummatches.album
+      console.log(ourresult)
+      res.json(ourresult);
+    })
+    .catch(err => console.log(err));
 }
 
 module.exports = search;
