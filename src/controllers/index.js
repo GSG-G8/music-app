5d14ee1f857ee563d1fb1dd3d8d8e202a701b1b0
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-02-19 13:38:22 +0200

    create render function relates #17

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
 
 const search = require('./search');
 const error = require('./error');
 
-router.post('/search', search)
-router.use(error.client)
-router.use(error.server)
+router.post('/search', search);
+router.use(error.client);
+router.use(error.server);
 
 
 module.exports = router;
-
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const app = require('./app');
 
 
 app.listen(app.get('port'), () => {
- console.log(`http://localhost:${app.get('port')}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`http://localhost:${app.get('port')}`);
+});
